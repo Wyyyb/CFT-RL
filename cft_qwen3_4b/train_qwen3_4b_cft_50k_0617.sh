7e6b807b6f585b428c67f1df1a4fb6a63c95d466
y726wang <ubo.wang.sunny@gmail.com>
2025-06-17 17:20:19 -0400

    add

--- cft_qwen3_4b/train_qwen3_4b_cft_50k_0617.sh
+++ cft_qwen3_4b/train_qwen3_4b_cft_50k_0617.sh
@@ -14,12 +14,12 @@ if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
 fi
 
-export CUDA_VISIBLE_DEVICES=6,7
+export CUDA_VISIBLE_DEVICES=0
 
 cd /map-vepfs/yubo/CriticCoT/ms-swift
 
 torchrun \
-    --nproc_per_node 2 \
+    --nproc_per_node 1 \
     --standalone \
     swift/cli/sft.py\
     --use_hf True \
@@ -30,12 +30,12 @@ torchrun \
     \
     --dataset $DATA_PATH \
     --split_dataset_ratio 0 \
-    --dataset_num_proc 2 \
+    --dataset_num_proc 1 \
     --streaming False \
     --strict False \
     --deepspeed zero3 \
     --remove_unused_columns False \
-    --dataloader_num_workers 2 \
+    --dataloader_num_workers 1 \
     \
     --truncation_strategy delete \
     \
@@ -50,7 +50,7 @@ torchrun \
     --logging_steps 1 \
     \
     --num_train_epochs 1 \
-    --gradient_accumulation_steps 256 \
+    --gradient_accumulation_steps 512 \
     --save_strategy "steps" \
     --save_steps 10 \
     --save_only_model True \
