4b6dde4198341e5c83197a7b9a88faaac5245727
y726wang <ubo.wang.sunny@gmail.com>
2025-06-22 15:53:01 -0400

    add

--- killarney_eval_0622/eval_qwen3_4b_base_rl.sh
+++ killarney_eval_0622/eval_qwen3_4b_base_rl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source /scratch/y726wang/verl-tool/.venv/bin/activate
+# source /scratch/y726wang/CFT-RL/Qwen2.5-Math-Eval-0203/.venv/bin/activate
+conda activate eval_math
 
 checkpoint_numbers=(20 40 60 80 100 120 140 160)
 # checkpoint_numbers=(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92 96 100 104 108 112 116 120 124 128 132 136 140 144 148 152 156 160 164 168 172 176 180 184 188 192 196 200)
@@ -10,7 +11,7 @@ export CUDA_VISIBLE_DEVICES=0
 # 串行处理每个检查点，每次都使用全部4张卡
 for ckpt_num in "${checkpoint_numbers[@]}"; do
     summary_path="/scratch/y726wang/CFT-RL/eval_results_qwen3_4b_base_rl/summary.txt"
-    saved_ckpt_path="/scratch/y726wang/CFT-RL/verl-data/save_checkpoints/cft-rl-base-0621_webinstruct-verified_Qwen3-4B-Base/global_step_${ckpt_num}"
+    saved_ckpt_path="/scratch/y726wang/CFT-RL/verl-data/save_checkpoints/cft-rl-base-0621_webinstruct-verified_Qwen3-4B-Base/global_step_${ckpt_num}/actor/"
     model_path="/scratch/y726wang/CFT-RL/verl-data/output_models/cft-rl_webinstruct-verified_Qwen3-4B-Base/ckpt-${ckpt_num}"
     output_path="/scratch/y726wang/CFT-RL/eval_results_qwen3_4b_base_rl/ckpt-${ckpt_num}/"
     INIT_MODEL_PATH="/scratch/y726wang/CFT-RL/verl-data/Qwen3-4B-Base"
