baaf62e2553b122803911f05863104201c506a2b
y726wang <ubo.wang.sunny@gmail.com>
2025-06-24 18:53:24 -0400

    add

---
+++ killarney_eval_0622/eval_qwen3_4b_cft_rl_mmlu-pro.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# source /scratch/y726wang/CFT-RL/Qwen2.5-Math-Eval-0203/.venv/bin/activate
+source /home/y726wang/projects/aip-wenhu/y726wang/miniconda3/bin/activate
+conda activate eval_math
+
+cd ../General-Reasoner
+
+python -m evaluation.eval_mmlupro \
+    --model_path /scratch/y726wang/CFT-RL/verl-data/qwen3_4b_cft_ckpt40 \
+    --output_file /scratch/y726wang/CFT-RL/verl-data/eval_results/output-mmlupro-qwen3-4b-cft_0624.json
+
